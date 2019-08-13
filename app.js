d4cf4aacb6eb9990137c43b4dbbcca731fed6c2a
Ajay Krishna reddy <ajay.krishna@iwebber.co.in>
2019-08-13 09:16:01 +0530

    passport authentication implemented

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
-
+const flash = require("connect-flash");
+const session = require("express-session");
+const passport = require("passport");
 const app = express();
-
+///passport config
+require("./config/passport")(passport);
+const connectDB = require("./config/db");
+//connect to db
+connectDB();
 //EJS
 app.use(expressLayouts);
 app.set("view engine", "ejs");
+// bodyparser
+app.use(express.urlencoded({ extended: false }));
+//Express sessions
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+//passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+// Global vars
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  next();
+});
 /* 
 routes config
 */
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,85 @@
 const express = require("express");
 const router = express.Router();
-
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const passport = require("passport");
 // Login page
-router.get("/login", (req, res) => res.send("Login"));
+router.get("/login", (req, res) => res.render("Login"));
 
 //Register
-router.get("/register", (req, res) => res.send("register"));
+router.get("/register", (req, res) => res.render("register"));
+
+//register handle
+router.post("/register", async (req, res) => {
+  //   return res.send(req.body);
+  const { name, email, password, password2 } = req.body;
+
+  let errors = [];
+
+  //check required  fields
+  if (!name || !email || !password || !password2) {
+    errors.push({ msg: "Please fill in all fields" });
+  }
+  //   check passwords match
+  if (password !== password2) {
+    errors.push({ msg: "Password do not match" });
+  }
+  //check pass length
+  if (password.length < 6) {
+    errors.push({ msg: "Password must be 6 characters" });
+  }
+  if (errors.length > 0) {
+    res.render("register", {
+      errors,
+      name,
+      email,
+      password,
+      password2
+    });
+  } else {
+    try {
+      //validation passed
+      let user = await User.findOne({ email });
+      errors.push({ msg: "Email already registerd" });
+      if (user) {
+        res.render("register", {
+          errors,
+          name,
+          email,
+          password,
+          password2
+        });
+      } else {
+        const newUser = new User({
+          name,
+          email,
+          password
+        });
+        let salt = await bcrypt.genSalt(10);
+        newUser.password = await bcrypt.hash(password, salt);
+        await newUser.save();
+        req.flash("success_msg", "You are now registered and can log in");
+        res.redirect("/users/login");
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }
+});
+
+//Login handle
+router.post("/login", (req, res, next) => {
+  passport.authenticate("local", {
+    successRedirect: "/dashboard",
+    failureRedirect: "/user/login",
+    failureFlash: true
+  })(req, res, next);
+});
+
+// Logout Handle
+router.get("/logout", (req, res) => {
+  req.logOut();
+  req.flash("success_msg", "You are Logged out");
+  res.redirect("/users/login");
+});
 module.exports = router;
